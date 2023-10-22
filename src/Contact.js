a1b8d0d42525936612c687416d14fa7e30a1d1f2
prakhar0034 <141361512+prakhar0034@users.noreply.github.com>
2023-10-23 00:22:26 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -4,9 +4,9 @@ const Contact = () => {
   return (
     <div className="section">
       <h2>Contact Me</h2>
-      <p>Email: aviral.jain2021a@vitstudetn.ac.in</p>
-      <p>Phone: 9588217939</p>
-      <p>LinkedIn:https://www.linkedin.com/in/aviral-jain-098992247</p>
+      <p>Email: prakhar.agarwal2021@vitstudent.ac.in</p>
+      <p>Phone: 8808240007</p>
+      <p>LinkedIn:https://https://www.linkedin.com/in/prakhar-agarwal-16181824b/</p>
     </div>
   );
 };
